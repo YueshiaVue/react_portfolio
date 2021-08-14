40e7c09487837fa38507648b553e2e5a53a96f78
Yueshia Vue <yueshiavue@Yueshias-MacBook-Pro.local>
2021-08-13 23:17:44 -0500

    Image testing

--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -6,15 +6,15 @@ function Portfolio () {
     <div>
         <h1>Portfolio</h1>
         <div class="image-box">
-            <img src="img/office.jpg" style={{width: '40%' }} class="hover image" alt="laptop on desk with open window view" />
+            <img src="./office.jpg" style={{width: '40%' }} class="hover image" alt="laptop on desk with open window view" />
             <a href="https://github.com/JacobBeckfeld/Cocktails-Cuisine-Craft-Beers">
-                <img src="img/work.jpg" style={{width: '40%' }} class="office overlay" alt="laptop on desk with open window view" />
+                <img src="./work.jpg" style={{width: '40%' }} class="office overlay" alt="laptop on desk with open window view" />
             </a>
         </div>
         <div class="image-box">
-            <img src="img/work.jpg" style={{width: '40%' }} class="hover image" alt="laptop on desk with open window view" />
+            <img src="./work.jpg" style={{width: '40%' }} class="hover image" alt="laptop on desk with open window view" />
             <a href="https://github.com/JacobBeckfeld/Group-Project_Two">
-                <img src="img/area.jpg" style={{width: '40%' }} class="office overlay" alt="laptop on desk with open window view" />
+                <img src="./area.jpg" style={{width: '40%' }} class="office overlay" alt="laptop on desk with open window view" />
             </a>
         </div>   
     </div>
