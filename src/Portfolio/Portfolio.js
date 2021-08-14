eae324533d71b9c2c29200c923dea1cd1ceedab8
Yueshia Vue <yueshiavue@Yueshias-MacBook-Pro.local>
2021-08-13 23:24:55 -0500

    Image testing

--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Portfolio.css';
-import work from './work.jpg'
+import work from './img/work.jpg'
 
 
 function Portfolio () {
@@ -8,15 +8,15 @@ function Portfolio () {
     <div>
         <h1>Portfolio</h1>
         <div class="image-box">
-            <img src="./office.jpg" style={{width: '40%' }} class="hover image" alt="laptop on desk with open window view" />
+            <img src="./img/office.jpg" style={{width: '40%' }} class="hover image" alt="laptop on desk with open window view" />
             <a href="https://github.com/JacobBeckfeld/Cocktails-Cuisine-Craft-Beers">
                 <img src={work} style={{width: '40%' }} class="office overlay" alt="laptop on desk with open window view" />
             </a>
         </div>
         <div class="image-box">
-            <img src="./work.jpg" style={{width: '40%' }} class="hover image" alt="laptop on desk with open window view" />
+            <img src="./img/work.jpg" style={{width: '40%' }} class="hover image" alt="laptop on desk with open window view" />
             <a href="https://github.com/JacobBeckfeld/Group-Project_Two">
-                <img src="./area.jpg" style={{width: '40%' }} class="office overlay" alt="laptop on desk with open window view" />
+                <img src="./img/area.jpg" style={{width: '40%' }} class="office overlay" alt="laptop on desk with open window view" />
             </a>
         </div>   
     </div>
