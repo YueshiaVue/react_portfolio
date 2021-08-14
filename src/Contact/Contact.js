efcf5c595c5eee43e19c4a38911dab32dd0da5d0
Yueshia Vue <yueshiavue@Yueshias-MacBook-Pro.local>
2021-08-13 20:17:08 -0500

    Added contact functions

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,30 +1,85 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Contact () {
-    return <div>
-        <h1>Contact</h1>
-        <form>
+    const [nameState,setNameState] = useState('');
+    const [nameEmail, setEmailState] = useState('');
+    const [nameMessage, setMessageState] = useState('');
+    const [submitSend,setSubmitSend] =useState(false);
+
+    const nameStateOnChange = (e) => {
+        let name = e.target.value
+        setNameState(name);
+    }
+
+    const nameEmailOnChange = (e) => {
+        let name = e.target.value
+        setEmailState(name);
+    }
+
+    const nameMessageOnChange = (e) => {
+        let name = e.target.value
+        setMessageState(name);
+    }
+    
+    const onSubmit = () => {
+        setNameState('')
+        setEmailState('')
+        setMessageState('')
+        setSubmitSend(true)
+        setTimeout(()=> {
+            setSubmitSend(false);
+        },1000)
+    };
+
+    return (
+        <div>
+        { submitSend ? <div style={{marginTop:'1em'}}>Complete</div> :
             <div>
-            <label>Name:</label>
-            </div>
-            <div>
-            <input type="text" id="name" ></input>
-            </div>
-            <div>
-            <label>Email Adress:</label>
-            </div>
-            <div>
-            <input type="text" id="email" ></input>
-            </div>
-            <div>
-            <label>Message:</label>
-            </div>
-            <div>
-            <textarea type="text" id="message" rows='4' cols='30'></textarea>
-            </div>
-            
-        </form>
-    </div>
+            <h1>Contact</h1>
+            <form>
+                <div>
+                    <label>Name:</label>
+                </div>
+                <div>
+                    <input
+                        type="text"
+                        id="name"
+                        value={nameState}
+                        onChange={nameStateOnChange}
+                    />
+                </div>
+                <div>
+                <label>Email Adress:</label>
+                </div>
+                <div>
+                    <input 
+                        type="text" 
+                        id="email" 
+                        value={nameEmail}
+                        onChange={nameEmailOnChange}
+                    />
+                </div>
+                <div>
+                <label>Message:</label>
+                </div>
+                <div>
+                <textarea 
+                type="text" 
+                id="message" 
+                rows='4' 
+                cols='30'
+                value={nameMessage}
+                onChange={nameMessageOnChange}
+                />
+                </div>
+            </form>
+            <button
+            onClick={onSubmit}
+            >Submit</button>
+        </div>
+        }
+        </div>
+    )
 }
 
 export default Contact
